0deb84aa40cefe7221c9b207cf325b01afb844c6
Om Shree <om22shree@gmail.com>
2022-06-21 15:02:42 +0530

    deleted trash

--- CTCI/Linked Lists/DeleteMiddleNode.cpp
+++
@@ -1,8 +0,0 @@
-bool deleteNode(Node *n) {
-    if(n == NULL || n->next == NULL) return false;
-    Node *next_node = n->next;
-    n->data = next_node->data;
-    n->next = next_node->next;
-    return true;
-}
-
--- Coding Ninjas CP C++/Complexity Assignment/findUnique.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-
-int findUnique(int *arr, int n) {
-	int res = arr[0];
-    	for (int i = 1; i < n; i++)
-        	res = res^arr[i];
- 
-    return res;
-}
