cb38bfa8570da6216f48f8a6021e2c2e77b089aa
Om Shree <om22shree@gmail.com>
2022-07-16 22:59:08 +0530

    Whatsup

---
+++ TCS NQT/Arrays/symmetricPairs.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int arr[5][2] = {{1, 2}, {2, 1}, {3, 4}, {5, 6}, {6, 5}};
+    unordered_map<int, int> m;
+    for (int i = 0; i < 5; i++) {
+        
+    }
+    return 0;
+}
