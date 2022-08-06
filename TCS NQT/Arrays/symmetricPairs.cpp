3b98c8d1f539ca30c3f6a6c9e4c82b798a75144b
Om Shree <om22shree@gmail.com>
2022-08-06 14:48:28 +0530

    symmetric pairs

--- TCS NQT/Arrays/symmetricPairs.cpp
+++ TCS NQT/Arrays/symmetricPairs.cpp
@@ -6,7 +6,14 @@ int main() {
     int arr[5][2] = {{1, 2}, {2, 1}, {3, 4}, {5, 6}, {6, 5}};
     unordered_map<int, int> m;
     for (int i = 0; i < 5; i++) {
-        
+        int first = arr[i][0];
+        int second = arr[i][1];
+
+        if(m.find(second)!=m.end() && m[second] == first) {
+            cout << "(" << first << ", " << second << "), ";
+        } else {
+            m[first] = second;
+        }
     }
     return 0;
 }
