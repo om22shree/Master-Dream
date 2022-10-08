8721ca2b8a315f89b1d2e8bc1be98b06ba18c692
Om Shree <om22shree@gmail.com>
2022-10-08 11:21:32 +0530

    SqrtDecomposition

---
+++ Basic Algorithms/SqrtDecomposition.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 10000
+#define SQRSIZE 100
+
+int arr[MAXN];		
+int block[SQRSIZE];		 
+int blk_sz;		
+
+void update(int idx, int val) {
+	int blockNumber = idx / blk_sz;
+	block[blockNumber] += val - arr[idx];
+	arr[idx] = val;
+}
+
+int query(int l, int r) {
+	int sum = 0;
+	while (l<r && l%blk_sz!=0 && l!=0) {
+		sum += arr[l];
+		l++;
+	}
+	while (l+blk_sz-1 <= r) {
+		sum += block[l/blk_sz];
+		l += blk_sz;
+	}
+	while (l<=r) {
+		sum += arr[l];
+		l++;
+	}
+	return sum;
+}
+
+void preprocess(int input[], int n) {
+	int blk_idx = -1;
+	blk_sz = sqrt(n);
+	for (int i=0; i<n; i++) {
+		arr[i] = input[i];
+		if (i%blk_sz == 0) {
+			blk_idx++;
+		}
+		block[blk_idx] += arr[i];
+	}
+}
+
+int main() {
+	int input[] = {1, 5, 2, 4, 6, 1, 3, 5, 7, 10};
+	int n = sizeof(input)/sizeof(input[0]);
+	preprocess(input, n);
+	cout << "query(3,8) : " << query(3, 8) << endl;
+	cout << "query(1,6) : " << query(1, 6) << endl;
+	update(8, 0);
+	cout << "query(8,8) : " << query(8, 8) << endl;
+	return 0;
+}
