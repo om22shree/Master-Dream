6fe0ef633b5c258fbf117a4994c8b8d53914ca2b
Om Shree <om22shree@gmail.com>
2022-10-16 17:14:24 +0530

    lcs added

--- rough.cpp
+++ rough.cpp
@@ -1,24 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> m;
-    for(int i = 0; i < nums.size(); i++) {
-        int x = target - nums[i];
-        if (m.find(x) != m.end()) {
-            return {i, m[x]};
-        }
-        m[nums[i]] = i;
-    }
-    return {};
-}
-
 int main() {
-    vector<int> nums = {1, 2, 3, 4};
-    int target = 7;
-    vector<int> ans = twoSum(nums, target);
-    for(auto it : ans)
-        cout << ans[it] << " ";
+    vector<int> v = {1, 2, 3, 4};
+    next_permutation(v.begin(), v.end());
+    for(auto it : v) {
+        cout << it << " ";
+    }
     cout << endl;
     return 0;
 }
