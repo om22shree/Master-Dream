985f196168015c6f81dfa77f1111410b9423c3a1
Om Shree <om22shree@gmail.com>
2022-10-18 19:34:43 +0530

    Merge overlapping subintervals

--- Good Questions/DSA Sheet/Arrays/MergeOverlapInterval.cpp
+++ Good Questions/DSA Sheet/Arrays/MergeOverlapInterval.cpp
@@ -1,3 +1,11 @@
+// Merge overlapping intervals to reduce the number of intervals.
+/*
+Logic :-
+    Sort the interval. Push the first interval to the answer array.
+    Iterate over the interval array if(interval[i][0] <= ans.back()[1])
+    then update ans.back()[1] = max(intervals[i][1], ans.back()[1]);
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,7 +15,8 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
     vector<vector<int>> ans;
     ans.push_back(intervals[0]);
     for(int i=1; i<intervals.size(); i++) {
-        if(ans.back()[1] >= intervals[i][0]) ans.back()[1] =                           max(ans.back()[1] , intervals[i][1]);
+        if(ans.back()[1] >= intervals[i][0]) 
+        ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
         else ans.push_back(intervals[i]); 
     }
     return ans;
