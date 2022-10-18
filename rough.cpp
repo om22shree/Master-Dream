3332cf7b218a5550b5edb88d1fa7ca8577f51e39
Om Shree <om22shree@gmail.com>
2022-10-18 23:31:54 +0530

    'sup

--- rough.cpp
+++ rough.cpp
@@ -1,2 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    auto it = lower_bound(nums.begin(), nums.end(), target);
+    vector<int> ans;
+    int x = it - nums.begin() + 1;
+    ans.push_back(x);
+    while(nums[x] == target)
+        x++;
+    ans.push_back(x);
+    return ans;
+}
